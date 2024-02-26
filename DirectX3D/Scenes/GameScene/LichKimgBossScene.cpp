f1884b6dd113475c7a35820dec154c78f2cc7a80
dedaia <89491270+dedaia@users.noreply.github.com>
2024-02-26 19:28:07 +0900

    승리 및 패배 UI 이미지 밑 버튼 이미지 추가

--- DirectX3D/Scenes/GameScene/LichKimgBossScene.cpp
+++ DirectX3D/Scenes/GameScene/LichKimgBossScene.cpp
@@ -10,7 +10,7 @@
 
 LichKimgBossScene::LichKimgBossScene()
 {
-	
+	map = new BossMap();
 }
 
 LichKimgBossScene::~LichKimgBossScene()
@@ -22,10 +22,46 @@ void LichKimgBossScene::Start()
 {
 	CH->PlayerSpawn(4);
 	//CH->GetPlayerData()->EquipWeapon(new Weapon("hammer_1", WeaponType::Hammer));
-	CH->GetPlayerData()->EquipWeapon(new Weapon("staff_1", WeaponType::Staff));
+	CH->GetPlayerData()->EquipWeapon(new Weapon("bow_1", WeaponType::Bow));
 	SKILL->Init(CH->GetPlayerData());
 
-	map = new BossMap();
+	vector<CH_Base_ver2*> Position_Select = CH->GetCharcterData();
+	float xxxx = -40;
+	float zzzz = 0;
+	float zzzz22 = 0;
+	int charcter_count = 0;
+	float nanum = 1;
+	float nujec = 0.5f;
+	Vector3 position_active;
+	for (int i = 0; i < Position_Select.size(); i++)
+	{
+		position_active = Vector3(xxxx, 0, zzzz);
+		Position_Select[i]->Pos() = position_active;
+		Position_Select[i]->Rot().y -= XM_PI * 0.5f;
+		Position_Select[i]->Update();
+
+		if (1 - nanum == 0)
+		{
+			charcter_count++;
+			nanum = 0;
+			xxxx += -5;
+			zzzz22 += 5;
+		}
+		else
+		{
+			if (nanum == 0)
+			{
+				zzzz = -zzzz22;
+			}
+			else
+			{
+				zzzz = zzzz22;
+			}
+			nanum += nujec;
+		}
+	}
+
+	
 	MONSTER->SpawnLickKing(Vector3());
 	if (Boss_LichKing* c = dynamic_cast<Boss_LichKing*>(MONSTER->GetLichKing()))
 	{
@@ -35,16 +71,6 @@ void LichKimgBossScene::Start()
 
 void LichKimgBossScene::Update()
 {
-	if (KEY_DOWN(VK_LEFT))
-	{
-		MONSTER->SpawnSkeleton(Vector3());
-		//MONSTER->SpawnIceBall(Vector3());
-	}
-	if (KEY_DOWN(VK_RIGHT))
-	{
-		MONSTER->SpawnSkeletonKnight(Vector3());
-	}
-
 	map->Update();
 
 	SKILL->Update();
